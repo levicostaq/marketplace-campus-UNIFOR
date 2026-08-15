c171db7285f39ec9bb54880d1deb13f66e4db1e8
Levi Costa <levicq06@gmail.com>
2026-08-15 16:09:23 -0300

    atualizando fetch

--- frontend/src/components/FormAnuncio.jsx
+++ frontend/src/components/FormAnuncio.jsx
@@ -31,7 +31,7 @@ function FormAnuncio({ onAnuncioCriado }) {
     setEnviando(true)
     setMensagem(null)
 
-    fetch("http://127.0.0.1:8000/anuncios", {
+    fetch(`${API_URL}/anuncios`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
